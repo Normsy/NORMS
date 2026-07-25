7b38d41616849d372b6863176a20d3876071a8bc
xovho <bazarte.clarkjohn06@gmail.com>
2026-07-25 15:23:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.querySelectorAll(".card").forEach(card=>{
 
+    card.addEventListener("mouseenter",()=>{
+
+        card.style.boxShadow="0 20px 40px rgba(0,0,0,.12)";
+
+    });
+
+    card.addEventListener("mouseleave",()=>{
+
+        card.style.boxShadow="0 10px 30px rgba(0,0,0,.05)";
+
+    });
+
+});
